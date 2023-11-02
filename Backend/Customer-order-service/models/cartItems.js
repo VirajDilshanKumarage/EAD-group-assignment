333463d4e79771ec2dd12ce9987d6f7897d9ed1f
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-11-02 08:47:40 +0530

    created model for cart

---
+++ Backend/Customer-order-service/models/cartItems.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema({
+  customerId: {
+    type: String,
+    required: true,
+  },
+  productId: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const CartItem = mongoose.model('CartItem', cartItemSchema);
+
+module.exports = CartItem;
