70a4b3ec2af84d53cb418d9ee20056e31d98b358
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-11-03 15:23:32 +0530

    removed update inventory function

--- Backend/Customer-order-service/services/InventoryManagerService.js
+++ Backend/Customer-order-service/services/InventoryManagerService.js
@@ -22,19 +22,6 @@ class InventoryService {
         }
     }
 
-    // Function to update inventory in the  inventory service (use this, if we need to update inventory with cart updates )
-    //   async updateInventory(productId, previousQuantity, newQuantity) {
-    //     try {
-    //       await axios.post(`${inventoryServiceBaseURL}/update-inventory/${productId}`, {
-    //         previousQuantity,
-    //         newQuantity,
-    //       });
-    //     } catch (error) {
-    //       console.error(error);
-    //       throw new Error('Error communicating with the inventory service');
-    //     }
-    //   }
-
 }
 
 module.exports = new InventoryService();
