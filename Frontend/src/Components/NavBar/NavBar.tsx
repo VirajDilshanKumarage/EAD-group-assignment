46995cec205fafac7d4e0bff14557100e9568a78
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-11-05 10:43:31 +0530

    added cart to the navbar

--- Frontend/src/Components/NavBar/NavBar.tsx
+++ Frontend/src/Components/NavBar/NavBar.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { FaUser, FaSignOutAlt } from 'react-icons/fa';
-import './NavBar.css'; 
+import { FaUser, FaSignOutAlt, FaCartPlus } from 'react-icons/fa';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
+
+  let customerId="customer123";
   return (
     <div className="navbar">
       <div className="logo">
         <span className="grocery-name">Royal Mall</span>
       </div>
       <div className="profile-icons">
-        <div className="icon">
+      <div className="icon" data-tooltip="Cart">
+          <Link to={`/customer-dashboard/add-to-cart/${customerId}`} className="icon custom-link" data-tooltip="Cart" >
+            <FaCartPlus />           
+          </Link>
+          </div>
+        <div className="icon" data-tooltip="User">
           <FaUser />
         </div>
-        <div className="icon">
+        <div className="icon" data-tooltip="Sign Out">
           <FaSignOutAlt />
         </div>
       </div>
