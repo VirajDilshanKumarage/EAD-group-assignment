cff5ead257f11504629f64373aa616e84720db5b
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-11-05 10:50:28 +0530

    fixed the navigate issue

--- Frontend/src/Components/NavBar/NavBar.tsx
+++ Frontend/src/Components/NavBar/NavBar.tsx
@@ -5,18 +5,19 @@ import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
 
-  let customerId="customer123";
+  let customerId = "customer123";
   return (
     <div className="navbar">
       <div className="logo">
         <span className="grocery-name">Royal Mall</span>
       </div>
       <div className="profile-icons">
-      <div className="icon" data-tooltip="Cart">
-          <Link to={`/customer-dashboard/add-to-cart/${customerId}`} className="icon custom-link" data-tooltip="Cart" >
-            <FaCartPlus />           
-          </Link>
+        <Link to={`/customer-dashboard/add-to-cart/${customerId}`} className="custom-link" data-tooltip="Cart" >
+          <div className="icon" data-tooltip="Cart">
+            <FaCartPlus />
           </div>
+        </Link>
+
         <div className="icon" data-tooltip="User">
           <FaUser />
         </div>
