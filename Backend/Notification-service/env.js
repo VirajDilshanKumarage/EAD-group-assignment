630dcea642c4b6bdf7dd4a6edc5e54d06e90d1a6
Chandula Kavishka Wijesekera <95069010+chandulakavishka@users.noreply.github.com>
2023-11-11 16:01:27 +0530

    add

--- Backend/Notification-service/env.js
+++ Backend/Notification-service/env.js
@@ -4,7 +4,7 @@ const emailConfig = {
   // EMAIL: 'projectmtool03@gmail.com',
   // PASSWORD: 'qmelsaacfcsgtpup',
   accountSid: 'ACc65b1a8724bc86a708215079b9b8f9a5',
-  authToken: 'a9029fe49672090f344cb04b1fcbe8f7', 
+  authToken: 'cb0bc54aaa54535e32652d65cb466474', 
   twilioNumber: '+12054481633'
 };
 
